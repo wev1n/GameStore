3aa1a7c412349fa8df9441a3d58bc4c3834a8e30
wevin <kevin.warme@gmail.com>
2024-06-16 14:35:23 +0200

    Change to using primary constructor on Game entity and dto

--- GameStore/Contracts/GameDTO.cs
+++ GameStore/Contracts/GameDTO.cs
@@ -1,20 +1,11 @@
 ﻿namespace GameStore.Contracts
 {
-    public class GameDTO
+    public class GameDTO(Guid id, string name, string genre, decimal price, DateOnly releaseDate)
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Genre { get; set; }
-        public decimal Price { get; set; }
-        public DateOnly ReleaseDate { get; set; }
-
-        public GameDTO(Guid id, string name, string genre, decimal price, DateOnly releaseDate)
-        {
-            Id = id;
-            Name = name;
-            Genre = genre;
-            Price = price;
-            ReleaseDate = releaseDate;
-        }
+        public Guid Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public string Genre { get; set; } = genre;
+        public decimal Price { get; set; } = price;
+        public DateOnly ReleaseDate { get; set; } = releaseDate;
     }
 }
