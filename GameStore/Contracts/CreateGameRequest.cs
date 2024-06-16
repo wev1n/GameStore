3f3ef31ecbcbb7cd3d34c44089c28cfaf2a48cdd
wevin <kevin.warme@gmail.com>
2024-06-16 16:35:27 +0200

    Add contoller for Game.cs for the usage of crud operations

---
+++ GameStore/Contracts/CreateGameRequest.cs
@@ -0,0 +1,43 @@
+﻿using GameStore.Data.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.Contracts
+{
+    public class CreateGameRequest(Guid id, string name, string genre, decimal price, DateOnly releaseDate, DateTime createdAt, string description, string publisher)
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; } = id;
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
+        public string Name { get; set; } = name;
+
+        [Required]
+        [MaxLength(30, ErrorMessage = "Genre cannot exceed 30 characters.")]
+        [OnlyLetters]
+        public string Genre { get; set; } = genre;
+
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Price must be between 0 and 1000.")]
+        [Column(TypeName = "decimal(3,2)")]
+        public decimal Price { get; set; } = price;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly ReleaseDate { get; set; } = releaseDate;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; } = createdAt;
+
+        [Required]
+        [MaxLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string Description { get; set; } = description;
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Publisher cannot exceed 100 characters.")]
+        public string Publisher { get; set; } = publisher;
+    }
+}
---
+++ GameStore/Controllers/GamesController.cs
@@ -0,0 +1,117 @@
+﻿using GameStore.Contracts;
+using GameStore.Data;
+using GameStore.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GamesController(ApplicationDbContext context) : ControllerBase
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        public IEnumerable<GameDTO> GetGames([FromQuery] string? name)
+        {
+            IEnumerable<Game> games = string.IsNullOrEmpty(name)
+                ? _context.Games.ToList()
+                : _context.Games.Where(x => x.Name == name);
+
+            IEnumerable<GameDTO> gameDtos = games.Select(x => new GameDTO
+            (
+                id: x.Id,
+                name: x.Name,
+                genre: x.Genre,
+                price: x.Price,
+                releaseDate: x.ReleaseDate
+            ));
+
+            return gameDtos;
+        }
+
+        [HttpGet("{name}")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(GameDTO), 200)]
+        [ProducesResponseType(404)]
+        public ActionResult<GameDTO> GetGameByName(string name)
+        {
+            var game = _context.Games.FirstOrDefault(x => x.Name == name);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            var gameDto = new GameDTO
+            (
+                id: game.Id,
+                name: game.Name,
+                genre: game.Genre,
+                price: game.Price,
+                releaseDate: game.ReleaseDate
+            );
+
+            return gameDto;
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(GameDTO), 201)]
+        [ProducesResponseType(400)]
+        public ActionResult<GameDTO> CreateGame(CreateGameRequest request)
+        {
+            var game = new Game
+            (
+                id: request.Id,
+                name: request.Name,
+                genre: request.Genre,
+                price: request.Price,
+                releaseDate: request.ReleaseDate,
+                createdAt: request.CreatedAt,
+                description: request.Description,
+                publisher: request.Publisher
+            );
+
+            _context.Games.Add(game);
+            _context.SaveChanges();
+
+            var gameDto = new GameDTO
+            (
+                id: game.Id,
+                name: game.Name,
+                genre: game.Genre,
+                price: game.Price,
+                releaseDate: game.ReleaseDate
+            );
+
+            return CreatedAtAction(
+                nameof(CreateGame),
+                new { id = game.Id },
+                gameDto
+            );
+        }
+
+        [HttpDelete("{id}")]
+        [Produces("application/json")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public ActionResult DeleteGame(Guid id)
+        {
+            var game = _context.Games.FirstOrDefault(x => x.Id == id);
+
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            _context.Games.Remove(game);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
